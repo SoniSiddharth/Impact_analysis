cf766d2dadb1021eaf6b04bc5d2da6d8456aa076
Siddharth Soni <56633798+SoniSiddharth@users.noreply.github.com>
2020-05-16 16:26:00 +0530

    Update update_emb.py

--- update_emb.py
+++ update_emb.py
@@ -4,19 +4,30 @@ import pandas as pd
 import tensorflow as tf
 import tensorflow_hub as hub
 import spacy
+
 nlp = spacy.load("en_core_web_sm")
-embed = hub.KerasLayer(r'C:\\Users\\Janvi Thakkar\\Desktop\\projects\\nlp_example')
+
+# calling the USE model
+embed = hub.KerasLayer(r'directory where model has been extracted')
+
+# a function for updating the embeddings of the test cases which have been updated
 def update_emb(path):
+# loading stored embeddings and test ids
     embeddings=np.loadtxt('embeddings.npz')
     id=np.loadtxt('id.npz')
+
     filename = os.path.basename(path)
     (file, ext) = os.path.splitext(filename)
     tst_id = int(file)
     index_in_embedding=0
+
+# searching for the index of updated test case 
     for i in range(0,len(id)):
         if(id[i]==tst_id):
             index_in_embedding = i 
             break 
+# creating the embeddings of the test case again using the USE model
+    embed = hub.KerasLayer("C:/Users/HP/Desktop/Capgemini", trainable= True)
     with open(path, 'r') as fp:
         t=[]
         lines = fp.read()
@@ -25,10 +36,11 @@ def update_emb(path):
         temprary = embed(t) 
         embeddings[index_in_embedding]=temprary[0]
         print('embeddings updated')
+
     np.savetxt('embeddings.npz',embeddings)
     np.savetxt('id.npz',id)
     print('embeddings saved')
 
 if __name__ == "__main__":
     print("Hi")
-    update_emb(r'C:\\Users\\Janvi Thakkar\\Desktop\\projects\\Capegemini\\separate_test_doc\\2293.txt')
+    update_emb(r'path of the text file of the altered test case')
