64531dbfcd3d2de2e4681e1848401558ff5c55b6
apk6601 <64188968+apk6601@users.noreply.github.com>
2020-05-17 14:02:30 +0530

    Update automate_up.py

--- automate_up.py
+++ automate_up.py
@@ -4,31 +4,29 @@ import time
 import pickle
 from datetime import datetime, timedelta
 
+#Citation: The basic framework for the watchdog observer has been taken from this link: https://www.geeksforgeeks.org/create-a-watchdog-in-python-to-look-for-filesystem-changes/
 
 class Handler(watchdog.events.PatternMatchingEventHandler): 
 	def __init__(self): 
-		# Set the patterns for PatternMatchingEventHandler
 		watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.pdf','*.docx','*.txt'], ignore_directories=True, case_sensitive=False) # Add the extensions as per your need
 		self.last_modified = datetime.now() 
+		
 	def on_created(self, event): 
-		print("Watchdog received created event - % s." % event.src_path) 
-		# print("Hello")
-		# Event is created, you can process it now 
+		print("Event is created - % s." % event.src_path) 
+		# print("Hello") 
 
 	def on_modified(self, event): 
 		if datetime.now() - self.last_modified < timedelta(seconds=1):
 			return 
 		else:
 			self.last_modified = datetime.now()
-		print("Watchdog received modified event - % s." % event.src_path) 
+		print("Event is modified - % s." % event.src_path) 
 		paths.append(event.src_path)
 		# update_emb(event.src_path)
-		# Event is modified, you can process it now
 
 	def on_deleted(self, event): 
-		print("Watchdog received deleted event - % s." % event.src_path) 
-		# print("Hello")
-		# Event is modified, you can process it now 
+		print("Event is deleted - % s." % event.src_path) 
+		# print("Hello") 
 
 
 if __name__ == "__main__": 
