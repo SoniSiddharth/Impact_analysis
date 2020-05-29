42f2de6c1b3e6104a31e8f430e5ad9330f7168f7
jvt3112 <64188154+jvt3112@users.noreply.github.com>
2020-05-29 14:24:19 +0530

    Updated embeddings_update.py

--- code_files/embeddings_update.py
+++ code_files/embeddings_update.py
@@ -1,15 +1,9 @@
 import pickle
-from update_emb import update_emb,embed
 from pre_process import creat_emb
 
 with open('mainfile.data','rb') as filehandler:
 			main = pickle.load(filehandler)  
 for j in range(len(main)):
     creat_emb(main[j]) 
-
-with open('listfile.data','rb') as filehandle:
-			paths = pickle.load(filehandle)
-for i in range(len(paths)):
-    update_emb(paths[i])
 
  
