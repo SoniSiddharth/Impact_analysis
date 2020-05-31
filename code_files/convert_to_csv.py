7ad6b64e30c561c6fd7fec5cde3a810287f2a3b9
apk6601 <64188968+apk6601@users.noreply.github.com>
2020-05-31 23:08:51 +0530

    Update convert_to_csv.py

--- code_files/convert_to_csv.py
+++ code_files/convert_to_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-def To_csv(output, search_string, results_returned):
+def To_csv(output, search_string, results_returned,threshold):
 	name = "Similar_to_" + str(search_string) + ".csv"
 	with open(name,'w',newline='') as ak:
 		AJS=csv.writer(ak)
@@ -10,6 +10,10 @@ def To_csv(output, search_string, results_returned):
 		AJS.writerow('\n')
 		AJS.writerow(['Number of results:'])
 		AJS.writerow([str(results_returned)])
+		AJS.writerow('\n')
+		AJS.writerow('\n')
+		AJS.writerow(['Your current threshold:'])
+		AJS.writerow([str(threshold)])
 		AJS.writerow('\n')
 		AJS.writerow('\n')
 		AJS.writerow(['Here are your similar Test IDs:'])
