e9dffa1543cfb9d18f5f0402e73bfdb16a836fa1
Siddharth Soni <56633798+SoniSiddharth@users.noreply.github.com>
2020-06-03 23:31:01 +0530

    Update embeddings_update.py

--- code_files/embeddings_update.py
+++ code_files/embeddings_update.py
@@ -8,11 +8,4 @@ embeddings=[]
 with open('mainfile.data','rb') as filehandler:
 			main = pickle.load(filehandler)  
 for j in range(len(main)):
-	if(j==0):
-    		creat_emb(main[j],id,embeddings)
-	else:
-		embeddings=np.loadtxt('embeddings.npz')
-		id=np.loadtxt('id.npz')
 		creat_emb(main[j],id,embeddings)
-
- 
