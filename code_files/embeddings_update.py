10551966fe99cfbf4117ceb41e2d97ccfc595aaa
jvt3112 <64188154+jvt3112@users.noreply.github.com>
2020-06-03 22:51:51 +0530

    Update embeddings_update.py

--- code_files/embeddings_update.py
+++ code_files/embeddings_update.py
@@ -7,6 +7,11 @@ embeddings=[]
 with open('mainfile.data','rb') as filehandler:
 			main = pickle.load(filehandler)  
 for j in range(len(main)):
-    creat_emb(main[j],id,embeddings) 
+	if(j==0):
+    		creat_emb(main[j],id,embeddings)
+	else:
+		embeddings=np.loadtxt('embeddings.npz')
+		id=np.loadtxt('id.npz')
+		creat_emb(main[j],id,embeddings)
 
  
