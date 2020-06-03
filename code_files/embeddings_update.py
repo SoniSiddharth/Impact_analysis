f17a122075e9692380d90b9cd2c4e9ed4c071da1
jvt3112 <64188154+jvt3112@users.noreply.github.com>
2020-06-03 22:47:54 +0530

    Update embeddings_update.py

--- code_files/embeddings_update.py
+++ code_files/embeddings_update.py
@@ -1,9 +1,12 @@
 import pickle
 from pre_process import creat_emb
 
+id=[]
+embeddings=[]
+
 with open('mainfile.data','rb') as filehandler:
 			main = pickle.load(filehandler)  
 for j in range(len(main)):
-    creat_emb(main[j]) 
+    creat_emb(main[j],id,embeddings) 
 
  
